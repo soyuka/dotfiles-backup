f71dfd9891983dc2bacbfdf264d94b7711cc26b0
soyuka <soyuka@gmail.com>
2014-11-01 11:56:04 +0100

    removed aptitude stuff

--- vimSetup.sh
+++ vimSetup.sh
@@ -1,5 +1,4 @@
 ## Vundle requires git and curl
-aptitude install git curl
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall #install vundle plugins
 mkdir -p ~/.vim/colors && cp ./Tomorrow-Night.vim ~/.vim/colors/Tomorrow-Night.vim
--- zshSetup.sh
+++ zshSetup.sh
@@ -1,4 +1,3 @@
-aptitude install zsh
 curl -L http://install.ohmyz.sh | sh
 cp .zshrc ~/.zshrc
 cp ./soyuka.zsh-theme ~/.oh-my-zsh/themes/
