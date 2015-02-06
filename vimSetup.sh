d1c16fe8760df7e9145751fd50c0bb1d2e353ebb
soyuka <soyuka@gmail.com>
2015-02-06 15:28:18 +0100

    Cp vimrc

--- vimSetup.sh
+++ vimSetup.sh
@@ -1,4 +1,5 @@
 ## Vundle requires git and curl
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/colors && cp ./Tomorrow-Night.vim ~/.vim/colors/Tomorrow-Night.vim
+[ ! -f ~/.vimrc ] && cp .vimrc ~/.vimrc || echo "vimrc already available"
 vim +PluginInstall +qall #install vundle plugins
