ff12904e95bc76b9b12059a57ddce46b38f9f87e
soyuka <soyuka@gmail.com>
2015-02-06 16:01:49 +0100

    Clone ycm

--- vimSetup.sh
+++ vimSetup.sh
@@ -2,4 +2,6 @@
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/colors && cp ./Tomorrow-Night.vim ~/.vim/colors/Tomorrow-Night.vim
 [ ! -f ~/.vimrc ] && cp .vimrc ~/.vimrc || echo "vimrc already available"
+git clone https://github.com/Valloric/YouCompleteMe ~/.vim/bundle/YouCompleteMe
 vim +PluginInstall +qall #install vundle plugins
+
